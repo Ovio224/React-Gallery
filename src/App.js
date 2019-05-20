ed78508d08ca5dc5498a48c9fddbae4f84b82db7
Ovi <covidiu97@gmail.com>
2019-05-20 10:43:57 +0200

    Small update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // dependencies
 import React, { Component } from 'react';
 import axios from 'axios';
-import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import apiKey from './config';
 import orderBy from 'lodash/orderBy';
 // components
@@ -11,46 +11,52 @@ import NotFound from './Components/NotFound';
 
 const invertDirection = {
   asc: 'desc',
-  desc: 'asc'
-}
+  desc: 'asc',
+};
 
 export default class App extends Component {
-
-    state = {
-      pics: [],
-      picsToBeLoaded: [],
-      loading: true,
-      sortDirection: 'desc',
-      counter: 1
-    }
+  state = {
+    pics: [],
+    picsToBeLoaded: [],
+    loading: true,
+    sortDirection: 'desc',
+    counter: 1,
+  };
 
   // fetching the data
   getPhotos = (query = 'beach') => {
-    this.setState({loading: true});
-    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&sort=interestingness-desc&per_page=24&format=json&nojsoncallback=1`)
+    this.setState({ loading: true });
+    axios
+      .get(
+        `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&sort=interestingness-desc&per_page=24&format=json&nojsoncallback=1`
+      )
       .then(response => {
         this.setState({
           pics: response.data.photos.photo,
-          loading: false
+          loading: false,
         }); // end setState
       }) // end then
       .catch(error => console.log(error));
-  }
+  };
 
-  handleSort(){
+  handleSort() {
     this.setState(prevstate => ({
-      sortDirection: prevstate.pics === this.state.pics ? invertDirection[this.state.sortDirection] : 'asc', 
-      pics: orderBy(this.state.pics, ['title'], this.state.sortDirection)
+      sortDirection: prevstate.pics === this.state.pics ? invertDirection[this.state.sortDirection] : 'asc',
+      pics: orderBy(this.state.pics, ['title'], this.state.sortDirection),
     }));
   }
 
-  getMorePics(query = 'beach'){
-    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&sort=interestingness-desc&per_page=24&page=${this.state.counter + 1}&format=json&nojsoncallback=1`)
+  getMorePics(query = 'beach') {
+    axios
+      .get(
+        `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&sort=interestingness-desc&per_page=24&page=${this
+          .state.counter + 1}&format=json&nojsoncallback=1`
+      )
       .then(response => {
         console.log(response);
         this.setState({
           counter: this.state.counter + 1,
-          pics: this.state.pics.concat(response.data.photos.photo)
+          pics: this.state.pics.concat(response.data.photos.photo),
         }); // end setState
       }) // end then
       .catch(error => console.log(error));
@@ -61,24 +67,28 @@ export default class App extends Component {
       // main container with routes
       <BrowserRouter>
         <div className="container">
-          <Route exact path="/search" render={({history}) => <Header history={history}/>}/>
+          <Route exact path="/search" render={({ history }) => <Header history={history} />} />
           <Switch>
-            <Route exact path="/search" 
-                render={({location}) => 
-                <GalleryForm 
+            <Route
+              exact
+              path="/search"
+              render={({ location }) => (
+                <GalleryForm
                   handleSort={this.handleSort.bind(this)}
                   getMorePics={this.getMorePics.bind(this)}
-                  location={location} 
-                  data={this.state.pics} 
-                  getPhotos={this.getPhotos} 
+                  location={location}
+                  data={this.state.pics}
+                  getPhotos={this.getPhotos}
                   key={location.key} // passing a unique key so componentDidMount mounts everytime the route changes !important
                   loadingState={this.state.loading}
-                />}/>
-            <Route exact path="/" render={() => <Redirect to="/search"/>}/>
+                />
+              )}
+            />
+            <Route exact path="/" render={() => <Redirect to="/search" />} />
             <Route component={NotFound} />
           </Switch>
         </div>
       </BrowserRouter>
-      );
+    );
   }
 }
--- src/Components/GalleryForm.js
+++ src/Components/GalleryForm.js
@@ -1,62 +1,75 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import queryString from 'query-string';
 import uniqid from 'uniqid';
 import GalleryItem from './GalleryItem';
 
 class GalleryForm extends Component {
-
   // change query string whenever a search occures
-  componentDidMount(){
+  componentDidMount() {
     const params = queryString.parse(this.props.location.search);
     this.props.getPhotos(params.tag);
   }
 
   handleClick = () => {
     const params = queryString.parse(this.props.location.search);
-      this.props.getMorePics(params && params.tag)
-  }
+    this.props.getMorePics(params && params.tag);
+  };
 
-  render(){
+  render() {
     const names = queryString.parse(this.props.location.search).tag;
     // displaying the pictures by passing the necessary props
     const results = this.props.data;
     let pictures;
-    if(results.length > 0) {
-      pictures = results.map(pic => 
+    if (results.length > 0) {
+      pictures = results.map(pic => (
         <GalleryItem
           farm={pic.farm}
           server={pic.server}
           key={uniqid()} // totally unecessary unique id dpdncy
           id={pic.id}
           secret={pic.secret}
-          title={pic.title} 
+          title={pic.title}
         />
-      ); // end map
+      )); // end map
     }
-     if(this.props.loadingState){
-      pictures = <img className="loading" src={(require('../assets/loading.gif'))} alt="Loading..." /> // loading gif
+    if (this.props.loadingState) {
+      pictures = <img className="loading" src={require('../assets/loading.gif')} alt="Loading..." />; // loading gif
     }
 
     return (
       <div className="photo-container">
-        { (names) ? // display the name of the tag
-        <h2>Pics of {names}s</h2>
-        : <h2>Pictures</h2>
-        }
-        <button className="btn-order" onClick={this.props.handleSort}>Order By Title</button>
+        {names ? ( // display the name of the tag
+          <h2>Pics of {names}s</h2>
+        ) : (
+          <h2>Pictures</h2>
+        )}
+        <button className="btn-order" onClick={this.props.handleSort}>
+          Order By Title
+        </button>
         <ul>
-          { (pictures) ? pictures : // display either the pictures or no results
-           <li className="not-found">
-            <h3>No Results Found</h3>
-            <p>Your search did not return any results. Please try again.</p>
-          </li>
-          }
+          {pictures ? (
+            pictures // display either the pictures or no results
+          ) : (
+            <li className="not-found">
+              <h3>No Results Found</h3>
+              <p>Your search did not return any results. Please try again.</p>
+            </li>
+          )}
         </ul>
-        <button className="btn-order" onClick={this.handleClick}>Show More</button>
-        { (names) ? 
-        <span className="footer">	Ovi &copy; Oh, I can see that you are liking <strong>{names}s</strong>, aren't you?</span>
-        : <span className="footer">  Hello there, I hope you like <strong>pictures</strong>. Go ahead, search for something!</span>
-        }
+        <button className="btn-order" onClick={this.handleClick}>
+          Show More
+        </button>
+        {names ? (
+          <span className="footer">
+            {' '}
+            Ovi &copy; Oh, I can see that you are liking <strong>{names}s</strong>, aren't you?
+          </span>
+        ) : (
+          <span className="footer">
+            {' '}
+            Hello there, I hope you like <strong>pictures</strong>. Go ahead, search for something!
+          </span>
+        )}
       </div>
     );
   }
